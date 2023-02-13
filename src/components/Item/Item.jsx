78e779fa18c3e3d396e60eab6ac388503c9d3e70
guille <guillermoneculqueo@gmail.com>
2023-02-13 02:15:48 -0300

    cambios 2

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,42 +1,50 @@
 import { Link } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import { useCartContext } from "../../context/CartContext";
 
 import 'react-toastify/dist/ReactToastify.css';
 
 const Item = ({element}) => {
 
-    const { agregaAlCarrito, cartList, contador } = useCartContext()
+    const { agregaAlCarrito, cartList, contadorComprados } = useCartContext()
 
     const cant = 1;
-    const añadirAlCart = (element, cant) => {
-        console.log(element);
-        console.log(element.nombre, cant);
 
-        agregaAlCarrito(
-            { ...element ,
-                comprado:cant, 
-            } )
-        notify(cant, element.nombre);
-        // agregarCarrito( {...objeto, cantidad:cant} );
-        
-    }
-
-    const notify = (numer, name) => toast.success(`🥰🥰 compraste ${numer} ${name}`, {
+    const notify = (cant, nombre) => toast.success(`🥰🥰 compraste ${cant} ${element.nombre}`, {
         position: "bottom-right",
         autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: true,
+        pauseOnHover: false,
         draggable: true,
         progress: undefined,
         theme: "light",
     });
 
+    const onAdd = (e) => {
+        e.preventDefault();
+        console.log(contadorComprados)
+        console.log(element.stock)
+
+        if ( contadorComprados < element.stock) {
+            console.log("entre")
+            agregaAlCarrito(
+                { ...element ,
+                    comprado:cant, 
+                } )
+    
+            notify(cant, element.nombre);   
+        }
+        // agregarCarrito( {...objeto, cantidad:cant} );
+    }
+
+
+
     const {id:id, nombre:nombre, precio:precio, descripcion:desc, imagenA:img1} = element;
     return (
         // <article key={id} className='card'>
-            <div key={id} className="containerContenido">
+        <>
+            <div className="containerContenido">
                 <Link to={`/detalle/${id} `} className="cardImg">
                     <img src={img1} alt={nombre} />
                     <p>${precio}</p>
@@ -49,11 +57,10 @@ const Item = ({element}) => {
                 <section className="botoneraCard">
                     <div>
                         <a ><ion-icon name="heart"></ion-icon></a>
-                        <a ><ion-icon name="cart"></ion-icon></a>
+                        <a onClick={onAdd}><ion-icon name="cart"></ion-icon></a>
                         <a ><ion-icon name="wallet"></ion-icon></a>
                     </div>
                 </section>
-
                 <ToastContainer
                         position="bottom-right"
                         autoClose={3000}
@@ -65,7 +72,9 @@ const Item = ({element}) => {
                         draggable
                         pauseOnHover
                         theme="light"/>
-            </div>
+            </div>      
+        </>
+            
             
         // </article>
     )
