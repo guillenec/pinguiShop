68eaaa9e675576301f9ca8781b21ae61c04d7af8
guille <guillermoneculqueo@gmail.com>
2023-02-25 19:42:18 -0300

    cambios slider 3

--- src/components/SimpleSlider/SimpleSlider.jsx
+++ src/components/SimpleSlider/SimpleSlider.jsx
@@ -22,20 +22,25 @@ const SimpleSlider = ( {objeto} ) => {
     return (
         <div className='containSlider'>
             <Slider {...settings}>
-                <div className='slider pokemon'>
+            <div className='sliderCaja dragonBall'>
+                    <img src="https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364406/imgs/slider/dragon_ball2_uwr8x6.jpg" alt="" />
                     <h3>1 Pokemon</h3>
                 </div>
-                <div className='slider onePunch'>
+                <div className='sliderCaja pokemon'>
+                    <img src="https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364421/imgs/slider/pokemon2_n5hntz.png" alt="" />
+                    <h3>1 Pokemon</h3>
+                </div>
+                <div className='sliderCaja onePunch'>
+                    <img src="https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364420/imgs/slider/one_punch_man2_qvxqfk.jpg" alt="" />
                     <h3>2 One punch</h3>
                 </div>
-                <div className='slider mandalorian'> 
+                <div className='sliderCaja mandalorian'> 
+                    <img src="https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364416/imgs/slider/mandalorian3_deualz.jpg" alt="" />
                     <h3>3 The mandalorian</h3>
                 </div>
-                <div className='slider onePiece'>
+                <div className='sliderCaja onePiece'>
+                    <img src="https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364422/imgs/slider/one_piece6_odxbmh.jpg" alt="" />
                     <h3>4 One Piece</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
                 </div>
             </Slider>
         </div>
