607e56f08d9cd62504c7e74761008cd98212c2ff
guille <guillermoneculqueo@gmail.com>
2023-02-26 05:36:06 -0300

    colecciones para el banner

--- src/components/SimpleSlider/SimpleSlider.jsx
+++ src/components/SimpleSlider/SimpleSlider.jsx
@@ -9,18 +9,19 @@ const SimpleSlider = ({ objeto }) => {
     console.log("Slider")
 
     const elementsBanner = [
-        { "id": 1, "nombre": "dragon ball", "descripcion": "banner logo dragon ball.", "palabraClave": "dragon ball", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364406/imgs/slider/dragon_ball2_uwr8x6.jpg", "color": "#EA6E14" },
-        { "id": 2, "nombre": "pokemon", "descripcion": "banner logo pokemon.", "palabraClave": "pokemonl", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677365775/imgs/slider/pokemonLogo1_ioxzjh.jpg", "color": "#0B48A5" },
+        { "id": 1, "nombre": "dragon ball", "descripcion": "banner logo dragon ball.", "palabraClave": "dragon ball", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364406/imgs/slider/dragon_ball2_uwr8x6.jpg", "color": "#EA6E14", "gradiente":"linear-gradient(0deg, rgba(235,68,15,1) 0%, rgba(233,154,27,1) 100%)" },
+        { "id": 2, "nombre": "pokemon", "descripcion": "banner logo pokemon.", "palabraClave": "pokemonl", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677365775/imgs/slider/pokemonLogo1_ioxzjh.jpg", "color": "#0B48A5", "gradiente":"" },
 
-        { "id": 3, "nombre": "one punch man", "descripcion": "banner logo dragon ball.", "palabraClave": "one punch man", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364420/imgs/slider/one_punch_man2_qvxqfk.jpg", "color": "#000000" },
+        { "id": 3, "nombre": "one punch man", "descripcion": "banner logo dragon ball.", "palabraClave": "one punch man", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364420/imgs/slider/one_punch_man2_qvxqfk.jpg", "color": "#000000", "gradiente":"" },
 
-        { "id": 4, "nombre": "the mandalorian", "descripcion": "banner the mandalorian.", "palabraClave": "the mandalorian", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364416/imgs/slider/mandalorian3_deualz.jpg", "color": "#345C8D" },
+        { "id": 4, "nombre": "the mandalorian", "descripcion": "banner the mandalorian.", "palabraClave": "the mandalorian", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364416/imgs/slider/mandalorian3_deualz.jpg", "color": "#000000", "gradiente":""  },
 
-        { "id": 5, "nombre": "one piece", "descripcion": "banner one piece.", "palabraClave": "one piece", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677366289/imgs/slider/onePieceLogo3_nfcomt.png", "color": "#51A1CB" },
+        { "id": 5, "nombre": "one piece", "descripcion": "banner one piece.", "palabraClave": "one piece", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677366289/imgs/slider/onePieceLogo1_cspj2h.jpg", "color": "#1C1924", "gradiente":"" },
 
-        { "id": 6, "nombre": "bad batch", "descripcion": "banner logo dragon ball.", "palabraClave": "bad batch", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364428/imgs/slider/bad_bach_guzdot.jpg", "color": "#cc5454" },
+        { "id": 6, "nombre": "bad batch", "descripcion": "banner logo bad batch.", "palabraClave": "bad batch", "imagen": "https://res.cloudinary.com/dpiwmbsog/image/upload/v1677364428/imgs/slider/bad_bach_guzdot.jpg", "color": "#cc5454", "gradiente":"linear-gradient(to right, #471E0C 0%, 49.070098996162415%, #D18261 100%)" },
     ]
 
+    
     const settings = {
         dots: true,
         infinite: true,
@@ -29,7 +30,7 @@ const SimpleSlider = ({ objeto }) => {
         slidesToScroll: 1,
         autoplay: true,
         cssEase: "linear",
-        autoplaySpeed: 5000,
+        autoplaySpeed: 55000,
     }
 
     return (
--- src/components/SliderItem/SliderItem.jsx
+++ src/components/SliderItem/SliderItem.jsx
@@ -4,17 +4,23 @@ import React, { useState, useEffect } from 'react';
 
 function SliderItem({ allProds, banner }) {
     const [predominantColor, setPredominantColor] = useState('#3E4098');
+    const [gradiente, setgradiente] = useState("null");
+
 
     useEffect(() => {
-        setPredominantColor(banner.color)
+        setPredominantColor(banner.color);
 
+        if (banner.gradiente !== "") {
+            setPredominantColor(banner.gradiente);
+        }  
     }, [])
 
-    const prodsBanner = allProds.filter((element) => banner.nombre === element.clave2 || banner.nombre === element.nombre || element.clave3 === banner.nombre)
+    const prodsBanner = allProds.filter((element) => banner.nombre === element.clave2 && element.clave3 === "coleccion")
         console.log(prodsBanner)
+        
     return (
         <>
-            <div className='sliderCaja dragonBall' style={{ backgroundColor: predominantColor }}>
+            <div className='sliderCaja dragonBall' style={{ background: predominantColor}}>
                 <img src={banner.imagen} alt="" />
                 <section className='contenidoSlider'>
                     <h3>funkos coleccio</h3>
@@ -26,3 +32,4 @@ function SliderItem({ allProds, banner }) {
 }
 
 export default SliderItem
+
