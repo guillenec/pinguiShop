f602feb8bebd671cdf9d4b744a2e1e8fded4bc74
guille <guillermoneculqueo@gmail.com>
2023-02-26 22:22:21 -0300

    algo se rompio en visual

--- src/components/SliderItem/SliderItem.jsx
+++ src/components/SliderItem/SliderItem.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // import Vibrant from 'node-vibrant';
 // import axios from 'axios';
 
 function SliderItem({ allProds, banner }) {
     const [predominantColor, setPredominantColor] = useState('#3E4098');
-    const [gradiente, setgradiente] = useState("null");
+    const [gradiente, setgradiente] = useState("null")
 
 
     useEffect(() => {
@@ -12,20 +12,24 @@ function SliderItem({ allProds, banner }) {
 
         if (banner.gradiente !== "") {
             setPredominantColor(banner.gradiente);
-        }  
+        }
     }, [])
 
-    const prodsBanner = allProds.filter((element) => banner.nombre === element.clave2 && element.clave3 === "coleccion")
-        console.log(prodsBanner)
-        
+    const prodsBanner = allProds.filter((element) => banner.nombre === element.clave2 && element.clave3 === "coleccion").slice(0, 5);
+    console.log(prodsBanner)
+
     return (
         <>
-            <div className='sliderCaja dragonBall' style={{ background: predominantColor}}>
-                <img src={banner.imagen} alt="" />
+            <div className='sliderCaja dragonBall' style={{ background: predominantColor }}>
+                <img src={banner.imagen} alt={banner.descripcion} />
                 <section className='contenidoSlider'>
-                    <h3>funkos coleccio</h3>
 
+                    <h3>funkos coleccio {banner.nombre}</h3>
+                    <section className='minisCard'>
+
+                    </section>
                 </section>
+
             </div>
         </>
     )
